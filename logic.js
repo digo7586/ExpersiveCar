31006d45021249b7b9b7e7935ccde5325e57a43d
Diego <digo7586@gmail.com>
2023-12-05 15:38:38 -0300

    Project Car

---
+++ logic.js
@@ -0,0 +1,71 @@
+function mudaimg () {    
+    let btnElements = document.querySelectorAll('.btncar,.logo-header');
+    
+    for (let i = 0; i < btnElements.length; i++) {
+        btnElements[i].style.color = '#fff'
+    }
+        document.getElementById('backg').style.backgroundImage="url(./img/nave.jpg)"
+        document.getElementById('txttitle').innerText = 'Jesko'
+        document.querySelector('.btncar:hover').style.color = '#E73C45';
+    }
+
+function mudaimg2 () { 
+    let btnElements = document.querySelectorAll('.btncar,.logo-header');
+        
+    for (let i = 0; i < btnElements.length; i++) {
+        btnElements[i].style.color = '#fff'
+    }
+
+    document.getElementById('backg').style.backgroundImage="url(./img/bmw.jpeg)";
+        document.getElementById('txttitle').innerText = 'Bmw'
+        document.querySelector('.btncar:hover').style.color = '#2A9E7D';
+
+    }
+
+function mudaimg3 () { 
+    let btnElements = document.querySelectorAll('.btncar,.logo-header');
+    
+    
+    for (let i = 0; i < btnElements.length; i++) {
+        btnElements[i].style.color = '#000'
+    }
+    
+        document.getElementById('backg').style.backgroundImage="url(./img/genesis.jpg)"
+        document.getElementById('txttitle').innerText = 'Genesis'  
+        document.querySelector('.btncar:hover').style.color = '#ED5F33';  
+    }
+
+function mudaimg4 () {  
+    let btnElements = document.querySelectorAll('.btncar,.logo-header');
+    
+    for (let i = 0; i < btnElements.length; i++) {
+        btnElements[i].style.color = '#fff'
+    }
+
+        document.getElementById('backg').style.backgroundImage="url(./img/puroSangue.jpg)"
+        document.getElementById('txttitle').innerText = 'Puro Sangue'
+        document.querySelector('.btncar:hover').style.color = '#E3B426';
+    }
+
+function mudaimg5 () { 
+    let btnElements = document.querySelectorAll('.btncar,.logo-header');
+    
+    for (let i = 0; i < btnElements.length; i++) {
+        btnElements[i].style.color = '#fff'
+    }
+
+        document.getElementById('backg').style.backgroundImage="url(./img/urus.jpg)"
+        document.getElementById('txttitle').innerText = 'Urus'
+        document.querySelector('.btncar:hover').style.color = '#77B0DA';
+    }
+    
+    function mudaimg6 () { 
+        let btnElements = document.querySelectorAll('.btncar,.logo-header');
+    
+    for (let i = 0; i < btnElements.length; i++) {
+        btnElements[i].style.color = '#fff'
+    }   
+        document.getElementById('backg').style.backgroundImage="url(./img/sporting.jpg)"
+        document.getElementById('txttitle').innerText = 'Sporting'
+        }
+
